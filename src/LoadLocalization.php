e0b71ea114174d46d536b9dce2cc6976c09d6f14
medilies <medilies@gmail.com>
2022-07-02 15:50:55 +0100

    getCurrentPathLang helper

--- src/LoadLocalization.php
+++ src/LoadLocalization.php
@@ -21,7 +21,37 @@ class LoadLocalization
             $localeLoader->MergeTranslations($jigsaw);
         };
 
+        $this->registerGetCurrentPathLangHelper($jigsaw);
         $this->registerTranslationRetrieverHelper($jigsaw);
+    }
+
+    private function registerGetCurrentPathLangHelper(Jigsaw $jigsaw)
+    {
+        $jigsaw->setConfig(
+            'getCurrentPathLang',
+            function ($page): string {
+
+                $path = $page->getPath();
+                $default_lang = $page->default_lang ?? 'en';
+
+                // Set $lang from path (4 cases)
+                if (!str_contains($path, '/')) {
+                    // index page
+                    if (empty($path))
+                        return $default_lang;
+                    else
+                        return $path;
+                } else {
+                    $lang = explode('/', $path)[1];
+
+                    // TODO: regex match 'xx' and 'xx_YY' lang codes
+                    if (!ctype_lower($lang) || strlen($lang) > 2)
+                        $lang = $default_lang;
+                }
+
+                return $lang;
+            }
+        );
     }
 
     private function registerTranslationRetrieverHelper(Jigsaw $jigsaw)
@@ -30,26 +60,7 @@ class LoadLocalization
             '__',
             function ($page, string $text, string|null $lang = null): string {
 
-                if (!$lang) {
-
-                    $path = $page->getPath();
-                    $default_lang = $page->default_lang ?? 'en';
-
-                    // Set $lang from path (4 cases)
-                    if (!str_contains($path, '/')) {
-                        // index page
-                        if (empty($path))
-                            $lang = $default_lang;
-                        else
-                            $lang = $path;
-                    } else {
-                        $lang = explode('/', $path)[1];
-
-                        // TODO: regex match 'xx' and 'xx_YY' lang codes
-                        if (!ctype_lower($lang) || strlen($lang) > 2)
-                            $lang = $default_lang;
-                    }
-                }
+                $lang = $lang ?? $page->getCurrentPathLang();
 
                 if (isset($page->$lang[$text]))
                     return $page->$lang[$text];
