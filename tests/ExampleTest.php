73cc463863a45d69ba10b76648e1cf70276397be
medilies <medilies@gmail.com>
2022-07-11 22:28:38 +0100

    phpunit setup

---
+++ tests/ExampleTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+final class ExampleTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function pass(): void
+    {
+        $this->assertTrue(true);
+    }
+}
